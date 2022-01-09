a028f19a1d1778ea00292cbba45fd772cabbd9f9
rafael747cardoso <rafael747cardoso@gmail.com>
2022-01-09 13:10:18 -0300

    ready to deploy

--- app.py
+++ app.py
@@ -839,9 +839,9 @@ def render_page_content(pathname):
 #----------------------------------------------------------------------------------------------------------------------
 ############################################## Run the dashboard ######################################################
 
-app.run_server(debug = True)
+# app.run_server(debug = True)
 
-# if __name__ == "__main__":
-#     app.run_server(debug = True)
+if __name__ == "__main__":
+    app.run_server(debug = True)
 
 
